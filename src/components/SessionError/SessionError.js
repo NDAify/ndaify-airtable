6cea3db42742d8d9e14a337d6d2cb24dcc570d51
Julia Qiu <julia@julia.dev>
2020-07-03 10:57:53 -0700

    fix SessionError

--- src/components/SessionError/SessionError.js
+++ src/components/SessionError/SessionError.js
@@ -1,10 +1,9 @@
 import React, { useEffect, useCallback } from 'react';
 import styled from 'styled-components';
 
+import * as Airtable from '@airtable/blocks/ui';
 import {
-  Box,
   useViewport,
-  Button,
 } from '@airtable/blocks/ui';
 
 import SettingsButton from '../SettingsButton/SettingButton';
@@ -47,40 +46,32 @@ const SessionError = () => {
   return (
     <>
       <SettingsButton show={false} />
-      <Box
-        position={viewport.isFullscreen ? 'absolute' : 'unset'}
-        top={0}
-        left={0}
-        right={0}
-        bottom={0}
-      >
-        <Box display="flex" height="100%" width="100%" flexDirection="column">
-          <Box display="flex" flexDirection="column" flex="1">
-            <Box padding="2pc 2pc 4pc 2pc" margin="0">
-              <NDAifyHeading style={{ paddingBottom: '8px' }}>
-                Invalid API Key
-              </NDAifyHeading>
-              <Paragraph style={{ paddingBottom: '2pc' }}>
-                Your API Key is no longer valid.
-                {' '}
-                You must reconfigure the block before you can continue.
-              </Paragraph>
+      <Airtable.Box>
+        <Airtable.Box height="calc(100vh - 80px)" overflow="scroll" display="flex" flexDirection="column" marginBottom="80px">
+          <Airtable.Box padding="2pc 2pc 4pc 2pc" margin="0">
+            <NDAifyHeading style={{ paddingBottom: '8px' }}>
+              Invalid API Key
+            </NDAifyHeading>
+            <Paragraph style={{ paddingBottom: '2pc' }}>
+              Your API Key is no longer valid.
+              {' '}
+              You must reconfigure the block before you can continue.
+            </Paragraph>
 
-            </Box>
-          </Box>
+          </Airtable.Box>
+        </Airtable.Box>
 
-          <Box padding="0 2pc" display="flex" justifyContent="flex-end" alignItems="center" height="80px" borderTop="thick">
-            <Pager numPages={2} activeIndex={0} />
-            <Button
-              onClick={onReconfigureClick}
-              variant="default"
-              size="large"
-            >
-              Reconfigure
-            </Button>
-          </Box>
-        </Box>
-      </Box>
+        <Airtable.Box backgroundColor="rgb(var(--ndaify-bg))" position="fixed" bottom="0" left="0" width="100%" padding="0 2pc" display="flex" justifyContent="flex-end" alignItems="center" height="80px" borderTop="thick">
+          <Pager numPages={2} activeIndex={0} />
+          <Airtable.Button
+            onClick={onReconfigureClick}
+            variant="default"
+            size="large"
+          >
+            Reconfigure
+          </Airtable.Button>
+        </Airtable.Box>
+      </Airtable.Box>
     </>
   );
 };
